638f9857a5c8b86c9f36aba7fbb937d5a9a10cb7
Praphul04 <praphulthakur6@gmail.com>
2024-01-31 12:13:35 +0530

    return custom responses

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=> {
-console.log("Praphul thakur")
+console.log(req.url, req.headers, req.method);
+res.setHeader('Content-Type', 'home/html');
+res.write('<html>');
+res.write('<head><title>My First page</title></head>');
+res.write('<body><h1>Welcome home</h1></body>');
+res.write('</html>');
+res.setHeader('Content-Type', 'about/html');
+res.write('<html>');
+res.write('<head><title>My First page</title></head>');
+res.write('<body><h1>Welcome to About Us page</h1></body>');
+res.write('</html>');
+res.setHeader('Content-Type', 'node/html');
+res.write('<html>');
+res.write('<head><title>My First page</title></head>');
+res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+res.write('</html>');
+res.end();
 })
 
-server.listen(4000);
+server.listen(3000);
