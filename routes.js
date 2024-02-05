f8de721d28add76cfa80367b39546fa91e8375b6
Praphul04 <praphulthakur6@gmail.com>
2024-02-05 20:54:46 +0530

    debugging

--- routes.js
+++ routes.js
@@ -29,9 +29,10 @@ const requestHandler = (req, res) => {
       });
     });
   }
+  
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
+  res.write('<head><title>My First Page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
